a0f1c1b1169abca75bbfcc48fbb15e7f7ec4e030
FRANCOIS <106148135+francoiszilmia@users.noreply.github.com>
2025-10-30 20:03:22 +0200

    Fix GitHub Actions workflow: Update to Java 17, improve headless mode, add better error handling

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -12,12 +12,31 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
+        // Check if running in CI environment
+        boolean isCI = System.getenv("CI") != null || System.getProperty("selenide.headless") != null;
+        
         // Selenide configuration
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = false;
+        Configuration.headless = isCI || Boolean.parseBoolean(System.getProperty("selenide.headless", "false"));
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 30000;
+        
+        // Additional Chrome options for CI
+        if (isCI) {
+            Configuration.browserCapabilities.setCapability("chrome.switches", new String[]{
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu",
+                "--disable-extensions",
+                "--disable-plugins",
+                "--disable-images",
+                "--disable-javascript-harmony-shipping",
+                "--disable-background-timer-throttling",
+                "--disable-renderer-backgrounding",
+                "--disable-backgrounding-occluded-windows"
+            });
+        }
         
         // Add Allure listener to Selenide
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
